7ee8d3207c874d97cc01f59e55bf45860318cd56
ilkovwork <ilkov.work@gmail.com>
2021-01-23 20:56:44 +0400

    add style to menu-burger

--- project3/js/tabs.js
+++ project3/js/tabs.js
@@ -37,5 +37,6 @@ $(document).ready(function() {
       $('.menu-burger').click(function() {
           $('.menu-burger').toggleClass('open-menu');
           $('ul').toggleClass('open-menu');
+          $('body').toggleClass('fixed-page');
       });
   });
